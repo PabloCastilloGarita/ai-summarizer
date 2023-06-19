b6dd53e14bf5707861edcf38a86ad00490b855ae
Pablo Castillo <pcastillogarita@gmail.com>
2023-06-19 15:58:52 -0600

    chore: configure ESLint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,11 @@
 module.exports = {
+  global: { module: false },
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
