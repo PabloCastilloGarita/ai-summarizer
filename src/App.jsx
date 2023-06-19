21041ff589e0e3fd4257808febd6ee355caeb963
Pablo Castillo <pcastillogarita@gmail.com>
2023-06-19 16:31:04 -0600

    feat: add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Hero from './components/Hero'
+import Demo from './components/Demo'
+import './App.css'
 
 const App = () => {
     return (
-        <div>App</div>
+        <div>
+            <Hero />
+            <Demo />
+        </div>
     )
 }
 
